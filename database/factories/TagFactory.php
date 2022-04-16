e8bedb0a3a80437b0276f336fe6ee584cec59293
Rudolf Bruder <rudolf.bruder@gmail.com>
2022-04-16 10:57:37 +0200

    Tag Seeder

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -17,7 +16,7 @@ class TagFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'title' => $this->faker->catchPhrase()
         ];
     }
 }
